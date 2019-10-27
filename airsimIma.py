81d262491b9ee4e432035581a1f1f7e77337320f
Dani <danfemu@hotmail.com>
2019-10-27 19:35:39 +0100

    Crear cliente y tomar imagenes

--- airsimIma.py
+++ airsimIma.py
@@ -1,30 +1,45 @@
 import airsim
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 
-client = airsim.MultirotorClient()
-client.confirmConnection()
-client.enableApiControl(True)
-client.armDisarm(True)
+# Devuelve el cliente de airsim para un multirotor
+def crearCliente():
+    client = airsim.MultirotorClient()
+    client.confirmConnection()
+    client.enableApiControl(True)
+    client.armDisarm(True)
+    return client
 
-# take images
-responses = client.simGetImages([
-    airsim.ImageRequest("0", airsim.ImageType.Scene, False, False),
-    airsim.ImageRequest("1", airsim.ImageType.DepthPlanner, True)])
-response = responses[0]
 
-# get numpy array
-img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8)
+# Toma como parámetro el cliente de airsim y devuelve una imagen de la
+# escena en formato np array BGR uint8
+def tomarImagen(client):
+    responses = client.simGetImages([
+        airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)])
+    response = responses[0]
+    img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8)
+    img_bgr = img1d.reshape(response.height, response.width, 3)
+    return img_bgr
 
-# reshape array to 4 channel image array H X W X 4
-img_rgb = img1d.reshape(response.height, response.width, 3)
 
-# original image is fliped vertically
-# img_rgb = np.flipud(img_rgb)
+def mostrarImagen(imaBGR):
+    plt.imshow(cv2.cvtColor(imaBGR, cv2.COLOR_BGR2RGB))
+    plt.show()
 
-# print(responses[0])
-plt.imshow(img_rgb)
-plt.show()
-# client.takeoffAsync().join()
-# client.moveToPositionAsync(-10, 10, -10, 5).join()
+
+if __name__ == "__main__":
+    client = crearCliente()
+
+    img_bgr = tomarImagen(client)
+    mostrarImagen(img_bgr)
+
+    client.takeoffAsync().join()
+    client.moveToPositionAsync(-10, 10, -10, 5).join()
+
+    img_bgr = tomarImagen(client)
+    mostrarImagen(img_bgr)
+
+    client.moveToPositionAsync(0, 0, 2, 5).join()
+    client.landAsync().join()
